069a9116a7ab824a955dfed51d1d1e8d7dc9c376
William Hass <wm.hass@gmail.com>
2019-09-20 09:48:24 -0300

    Creating comics list view mock and setting up comics list presenter suite

--- BDDComicsListMacOSTests/Comics List/ComicsListPresenterSpecs.swift
+++ BDDComicsListMacOSTests/Comics List/ComicsListPresenterSpecs.swift
@@ -18,21 +18,28 @@ import Nimble
 
 class ComicsListPresenterSpecs: QuickSpec {
     
+    var presenter: ComicsListPresenter!
+    var viewMock: ComicsListViewMock!
+    
     override func setUp() {
         beforeSuite {
+            let viewMock = ComicsListViewMock()
+            let presenter = ComicsListPresenter()
+            presenter.view = viewMock
             
+            self.viewMock = viewMock
+            self.presenter = presenter
         }
-        
         describe("ComicsListPresenter") {
             
             context("When it is asked to presentResponseIsInvalid") {
-                it("Should ask view to display a message saying...") {
+                it("Should ask view to display an alert informing that there was an error with the server response") {
                     
                 }
             }
             
             context("When it is asked to presentNoInternetConnectionErrorMessage") {
-                it("Should ask view to display a message saying...") {
+                it("Should ask view to display an alert informing that there is no internet connection") {
                     
                 }
             }
---
+++ BDDComicsListMacOSTests/Mocks/ComicsListViewMock.swift
@@ -0,0 +1,17 @@
+//
+//  ComicsListViewMock.swift
+//  BDDComicsListMacOSTests
+//
+//  Created by William Hass on 2019-09-20.
+//  Copyright © 2019 William. All rights reserved.
+//
+
+import Foundation
+
+class ComicsListViewMock {
+    
+}
+
+extension ComicsListViewMock: ComicsListDisplayLogic {
+    
+}
