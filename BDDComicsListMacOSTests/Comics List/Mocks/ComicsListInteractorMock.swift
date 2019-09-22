8cd42d5e7a0b326e051ac0bfafc09c8cfc00668e
William Hass <wm.hass@gmail.com>
2019-09-22 00:32:00 -0300

    creating comics list interaction mock

---
+++ BDDComicsListMacOSTests/Comics List/Mocks/ComicsListInteractorMock.swift
@@ -0,0 +1,22 @@
+//
+//  ComicsListInteractorMock.swift
+//  BDDComicsListMacOSTests
+//
+//  Created by William Hass on 2019-09-22.
+//  Copyright © 2019 William. All rights reserved.
+//
+
+import Foundation
+
+class ComicsListInteractorMock {
+    
+}
+
+extension ComicsListInteractorMock: ComicsListBusinessLogic {
+    func loadListOfComics() {
+        
+    }
+    func comicSelected(comic: Comic) {
+        
+    }
+}
