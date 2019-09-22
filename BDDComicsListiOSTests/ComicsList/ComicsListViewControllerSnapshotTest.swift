ee5112a790cbfb7f48ae41be0b3f9df57c364159
William Hass <wm.hass@gmail.com>
2019-09-21 22:57:38 -0300

    added snapshot tests to the comics list view controller

---
+++ BDDComicsListiOSTests/ComicsList/ComicsListViewControllerSnapshotTest.swift
@@ -0,0 +1,73 @@
+//
+//  ComicsListViewControllerSnapshotTest.swift
+//  BDDComicsListiOSTests
+//
+//  Created by William Hass on 2019-09-21.
+//  Copyright © 2019 William. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import UIKit
+import XCTest
+@testable import BDDComicsList
+
+
+class ComicsListViewControllerSnapshotTest: FBSnapshotTestCase {
+    
+    var comicsListViewController: ComicsListViewController!
+    var window: UIWindow!
+
+    override func setUp() {
+        super.setUp()
+        self.recordMode = false
+
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let initialViewController = storyBoard.instantiateInitialViewController() as? UINavigationController
+        let comicsListViewController = initialViewController?.viewControllers.first as! ComicsListViewController
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.makeKeyAndVisible()
+        window.rootViewController = initialViewController
+        
+        self.window = window
+        self.comicsListViewController = comicsListViewController
+        
+        initialViewController?.view.setNeedsLayout()
+        initialViewController?.view.layoutIfNeeded()
+    }
+    
+    func testComicsListViewControllerWithComics() {
+        let comics = [
+            Comic(id: 123, title: "aaa"),
+            Comic(id: 231, title: "baa"),
+            Comic(id: 231, title: "bba"),
+            Comic(id: 231, title: "bbb"),
+        ]
+        let groupedSortedComics = GroupedSortedComics(comics: comics)
+        let viewModel = ComicsListViewModel(groupedComics: groupedSortedComics)
+        self.comicsListViewController.displayComics(viewModel: viewModel)
+        
+        self.snapshotVerifyView(self.comicsListViewController.view, withIdentifier: "")
+    }
+    
+    func testComicsListViewControllerWithoutComics() {
+        let groupedSortedComics = GroupedSortedComics(comics: [])
+        let viewModel = ComicsListViewModel(groupedComics: groupedSortedComics)
+        self.comicsListViewController.displayComics(viewModel: viewModel)
+        
+        self.snapshotVerifyView(self.comicsListViewController.view, withIdentifier: "")
+    }
+    
+    func testComicsListViewControllerDisplayingError() {
+        self.comicsListViewController.displayErrorAlert(title: "Error title", message: "Error message")
+        self.snapshotVerifyView(self.comicsListViewController.presentedViewController?.view, withIdentifier: "")
+    }
+    
+    func testComicsListViewControllerDisplayingActivityIndicator() {
+        self.comicsListViewController.displayUIActivityView(true)
+        self.snapshotVerifyView(self.comicsListViewController.view, withIdentifier: "")
+    }
+    
+}
--- README.md
+++ README.md
@@ -169,8 +169,9 @@ When wirting tests we don't write any view (i.e. UIKit dependent classes). This 
     h) It is okay that user stories change along the development: The developer should never guess, if you realize that something is missing, add first in the user stories before implement it.
     i) Write your code based on behaviour
     j) Write unit tests if needed, but in a BDD style: 2c14ab14de5267bda9a6198871db1bbbc371a329
-    k) Write viewcontroller tests in iOSTests target - it has access to uikit
-    l) You do not write any code in the application without a test - you need to justify why you are writing any line 
+    k) Write viewcontroller and custom list table view cell tests in iOSTests target - it has access to uikit
+    l) You do not write any code in the application without a test - you need to justify why you are writing any line
+    m) Added snapshot tests to custom list table view cell: 857ba0b77993084c70687f0bb626a86cc4d3936e
     
 2. Create viewcontroller and views/snapshot tests
 
